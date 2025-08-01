fee5f3d51d231fa8c3050fb1150dd80c3130d674
Ebraam Sobhy <142206373+EbraamSobhy@users.noreply.github.com>
2025-08-01 10:08:25 +0300

    add nodemon

--- Nodejs.sh
+++ Nodejs.sh
@@ -10,6 +10,9 @@ npm init -y
 # Install Express
 npm install express
 
+# Install Nodemon as a development dependency
+npm install --save-dev nodemon
+
 # Create the main application file
 echo "const express = require('express');
 const app = express();
@@ -28,9 +31,8 @@ app.listen(port, () => {
   console.log(\`Server is running at http://localhost:\${port}\`);
 });" > index.js
 
-# Run the App
-node index.js
+# Update package.json to use nodemon for development
+npx json -I -f package.json -e 'this.scripts.dev="nodemon index.js"'
 
-# The server will be running at http://localhost:3000
-
-
+# Run the App using Nodemon
+npx nodemon index.js
